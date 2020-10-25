c32acc907368b7a557b67dca205bde4e1a8b52f0
Mike Evlantev <mikeev21@gmail.com>
2020-10-25 12:59:24 -0700

    moved connecting to mongodb gridfs out of server.js

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Grid = require('gridfs-stream');
+require("dotenv").config();
+
+const connectDb = () => {
+  try {
+    // Create connection
+    const conn = mongoose.createConnection(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    // Init gfs
+    let gfs;
+    conn.once('open', () => {
+      gfs = Grid(conn.db, mongoose.mongo);
+      gfs.collection(process.env.UPLOADS_COLLECTION_NAME); // uploads.chunks and uploads.files
+    });
+    console.log("MongoDB GridFS Connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;
