a9c262edd188ad18bf29efc8eeabe78cd1ee3ad0
Mike Evlantev <mikeev21@gmail.com>
2020-10-25 12:58:41 -0700

    moved uploads routes out of server.js

---
+++ routes/upload.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const upload = require('../middleware/upload');
+
+// @route   GET /
+// @desc    Form to select file
+// @access  Public
+router.get('/', (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname + '../../index.html'));
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   POST /upload
+// @desc    Upload file to DB
+// @access  Public
+router.post('/upload', upload.single('file'), (req,res) => { // 'file' mathces the input type file with name 'file'. See index.html
+  try {
+    res.json({file: req.file});
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
